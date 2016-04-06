1a6eab9fbd8f1b147a7d5f773583daded8c9c612
William Ku <kucheech@gmail.com>
2016-04-05 20:25:24 -0700

    added OnItemClickListener

--- app/src/main/java/sg/edu/tmc/comp1661/listviewexample/MainActivity.java
+++ app/src/main/java/sg/edu/tmc/comp1661/listviewexample/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.tmc.comp1661.listviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -28,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
         // Attach the adapter to a ListView
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Event e = (Event) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, "Selected: " + e.getTitle(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
